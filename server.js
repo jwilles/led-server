9630b35f036ac48a71770bd7e3e70fe84e6c0b8e
John <johnwilles@gmail.com>
2018-01-28 11:01:02 -0500

    Remove unused module requirements

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const request = require('request');
-const requestPromise = require('request-promise-native');
-const xml2js = require('xml2js');
-const xmlParser = xml2js.parseString;
 const app = express();
 
 const routes = require('./app/routes');
-
 const PORT = process.env.PORT || 3000
 
 app.use('/', routes);
